92122d2873c8cfe81ecbaf621e440bbddcf714a6
vinlee19 <1401597760@qq.com>
2024-02-18 15:21:13 +0800

    modify CdcTools

--- flink-doris-connector/src/main/java/org/apache/doris/flink/tools/cdc/db2/Db2Schema.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/tools/cdc/db2/Db2Schema.java
@@ -1,10 +1,11 @@
 package org.apache.doris.flink.tools.cdc.db2;
 
+import org.apache.doris.flink.tools.cdc.JdbcSourceSchema;
 import org.apache.doris.flink.tools.cdc.SourceSchema;
 
 import java.sql.DatabaseMetaData;
 
-public class Db2Schema extends SourceSchema {
+public class Db2Schema extends JdbcSourceSchema {
     public Db2Schema(
             DatabaseMetaData metaData,
             String databaseName,
@@ -12,11 +13,16 @@ public class Db2Schema extends SourceSchema {
             String tableName,
             String tableComment)
             throws Exception {
-        super(metaData,null, schemaName, tableName, tableComment);
+        super(metaData, databaseName, null, tableName, tableComment);
     }
 
     @Override
     public String convertToDorisType(String fieldType, Integer precision, Integer scale) {
         return Db2Type.toDorisType(fieldType, precision, scale);
+    }
+
+    @Override
+    public String getCdcTableName() {
+        return "";
     }
 }
